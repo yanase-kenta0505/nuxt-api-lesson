2e46257aeb5c0a9a995ff2dba4ce9e773cb3378a
yanasekenta <ynsknt0505@gmail.com>
2022-08-19 06:30:40 +0900

    フロント側からapiを叩く

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,20 @@
 <template>
-
+  <div>
+    {{ message }}
+  </div>
 </template>
 
 <script>
+import { defineComponent, useAsync, useContext } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const { $axios } = useContext()
+    const message = useAsync(() => $axios.$get('http://localhost:3000/api'))
+    
+    return {
+      message
+    }
+  }
+})
 </script>
